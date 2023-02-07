f6ce3f6fc0dffbc70311d3b1424b58593905d07a
BLANK <67583052+nikkynoddy12@users.noreply.github.com>
2023-02-07 20:52:11 +0530

    Add files via upload

---
+++ ReplaceAllExample.java
@@ -0,0 +1,9 @@
+package javaunit7;
+
+
+public class ReplaceAllExample{
+    public static void main(String args[]){
+        String s1="javatpoint is a very good website";
+        String replaceString=s1.replaceAll("a","e");
+        System.out.println(replaceString);
+    }}
---
+++ ReplaceExample.java
@@ -0,0 +1,7 @@
+package javaunit7;
+public class ReplaceExample{
+    public static void main(String args[]){
+        String s1="my name is khan my name is java";
+        String replaceString=s1.replace("is","was");
+        System.out.println(replaceString);
+    }}
